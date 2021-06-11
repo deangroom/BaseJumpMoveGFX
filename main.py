7182cd494bffa431efcf073363af955226315ffe
outttatime <deangroom@gmail.com>
2021-06-11 02:56:14 +0000

    added comments

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import pygame
 pygame.init()
 
+###---setting up the window size using variables---###
+x=0 #initialise x
+y=0 #initialise y
+
 winx = 500
 winy = 300
 win = pygame.display.set_mode((winx,winy))
-pygame.display.set_caption("First Game")
+pygame.display.set_caption("Left, Right and Jump")
 
-x = 150 #position of rect
-y = 290 #position of rect
+###---positioning the rect in the middle
+
+
 width = 10 #sie of rect
 height = 10 #size of rect
+
+###--- position rect in middle and at the bottom---###
+x = 250 #position of rect
+y = 250  #position of rect
+
+
 vel = 5 #velocity or attack value
+jumpMax = 10 #set the number of iterations for the jump maths
+
+### --- this is just to show you how abs (absolute value) works ###
+### --- the absolute value of 20 is -20@
+integer = -20
+print('Absolute value of -20 is:', abs(integer))
 
 isJump = False #start the routine in jump flag set to false
-jumpCount = 10 # number of iterations for the jump loop.
+jumpCount = jumpMax # number of iterations for the jump loop.
 
 run = True
 
@@ -43,11 +60,13 @@ while run:
         if keys[pygame.K_SPACE]:
             isJump = True
     else:
-        if jumpCount >= -10:
+        if jumpCount >= -jumpMax:
             y -= (jumpCount * abs(jumpCount)) * 0.5
-            jumpCount -= 1
+            print('jump grav', y) #show the maths
+            jumpCount -= 1 #take away one from the loop
+            print ('rect position',(jumpCount - y))
         else: 
-            jumpCount = 10
+            jumpCount = jumpMax
             isJump = False
     
     win.fill((0,0,0))
